8559becb4db59b1735ad9c22b77f2d0ca364b694
rdstonech <rdstonech@gmail.com>
2017-11-03 18:53:04 +0300

    Fix McpeLevelEvent

--- server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeLevelEvent.java
+++ server/src/main/java/com/voxelwind/server/network/mcpe/packets/McpeLevelEvent.java
@@ -1,6 +1,6 @@
 package com.voxelwind.server.network.mcpe.packets;
 
-import com.flowpowered.math.vector.Vector3i;
+import com.flowpowered.math.vector.Vector3f;
 import com.voxelwind.nbt.util.Varints;
 import com.voxelwind.server.network.NetworkPackage;
 import com.voxelwind.server.network.mcpe.McpeUtil;
@@ -11,14 +11,14 @@ import lombok.Data;
 public class McpeLevelEvent implements NetworkPackage
 {
 	private int eventId;
-	private Vector3i position;
+	private Vector3f position;
 	private int data;
 	
 	@Override
 	public void decode (ByteBuf buffer)
 	{
 		eventId = Varints.decodeSigned (buffer);
-		position = McpeUtil.readBlockCoords (buffer);
+		position = McpeUtil.readVector3f (buffer);
 		data = Varints.decodeSigned (buffer);
 	}
 	
@@ -26,7 +26,7 @@ public class McpeLevelEvent implements NetworkPackage
 	public void encode (ByteBuf buffer)
 	{
 		Varints.encodeSigned (buffer, eventId);
-		McpeUtil.writeBlockCoords (buffer, position);
+		McpeUtil.writeVector3f (buffer, position);
 		Varints.encodeSigned (buffer, data);
 	}
 }
